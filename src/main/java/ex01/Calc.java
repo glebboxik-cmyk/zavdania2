01522aef6e99c52a66134e3ee01b249d23333f55
glebboxik-cmyk <glebboxik@gmail.com>
2026-03-11 13:38:36 +0200

    Add calculation and file operations to Calc class
    Implement calculation, saving, and restoring of results in Calc class.

--- src/main/java/ex01/Calc.java
+++ src/main/java/ex01/Calc.java
@@ -1,4 +1,32 @@
 package ex01;
 
+import java.io.*;
+
 public class Calc {
+    private Item2d result;
+    private String file = "save.bin";
+
+    public void calculate(double m, double v, double h) {
+        double e = (m * v * v) / 2 + (m * 9.81 * h);
+        result = new Item2d(m, v, h, e);
+    }
+
+    public void save() throws IOException {
+        ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(file));
+        out.writeObject(result);
+        out.close();
+    }
+
+    public void restore() throws Exception {
+        ObjectInputStream in = new ObjectInputStream(new FileInputStream(file));
+        result = (Item2d) in.readObject();
+        in.close();
+        result.bin = Long.toBinaryString((long) result.energy);
+    }
+
+    public void show() {
+        if (result != null) System.out.println(result.toString());
+    }
+
+    public Item2d getResult() { return result; }
 }
