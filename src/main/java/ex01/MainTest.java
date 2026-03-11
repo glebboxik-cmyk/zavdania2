7766889dffe7c042791722e3699915dc9f9ce33c
glebboxik-cmyk <glebboxik@gmail.com>
2026-03-11 13:39:22 +0200

    Implement main method for calculation testing
    Added a main method to execute a calculation test.

--- src/main/java/ex01/MainTest.java
+++ src/main/java/ex01/MainTest.java
@@ -1,4 +1,14 @@
 package ex01;
 
 public class MainTest {
+    public static void main(String[] args) {
+        Calc c = new Calc();
+        c.calculate(1, 0, 10);
+
+        if (Math.abs(c.getResult().energy - 98.1) < 0.1) {
+            System.out.println("Test ok");
+        } else {
+            System.out.println("Pomylka testu");
+        }
+    }
 }
