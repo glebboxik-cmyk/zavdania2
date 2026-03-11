90e26c8552aae6c1b8f904b0861e84bd5b4cfe21
glebboxik-cmyk <glebboxik@gmail.com>
2026-03-11 13:38:58 +0200

    Implement main method for calculation functionality

--- src/main/java/ex01/Main.java
+++ src/main/java/ex01/Main.java
@@ -1,4 +1,18 @@
 package ex01;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) throws Exception {
+        Calc c = new Calc();
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Vvedit m, v, h: ");
+        c.calculate(s.nextDouble(), s.nextDouble(), s.nextDouble());
+
+        c.show();
+        c.save();
+
+
+    }
 }
